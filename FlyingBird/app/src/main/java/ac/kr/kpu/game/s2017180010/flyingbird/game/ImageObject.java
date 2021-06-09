66c09411c310ae1f5187ae7dfb02479a0bad1581
mwon0326 <mwon0326@gmail.com>
2021-06-09 22:28:40 +0900

    ImageObject 추가

---
+++ FlyingBird/app/src/main/java/ac/kr/kpu/game/s2017180010/flyingbird/game/ImageObject.java
@@ -0,0 +1,29 @@
+package ac.kr.kpu.game.s2017180010.flyingbird.game;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.RectF;
+
+import ac.kr.kpu.game.s2017180010.flyingbird.framework.GameBitmap;
+import ac.kr.kpu.game.s2017180010.flyingbird.framework.GameObject;
+
+public class ImageObject implements GameObject {
+    private Bitmap bitmap;
+    private RectF dstRect = new RectF();
+
+    public ImageObject(int left, int top, int right, int bottom, int resId)
+    {
+        bitmap = GameBitmap.load(resId);
+        dstRect.set(left, top, right, bottom);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, null, dstRect, null);
+    }
+}
